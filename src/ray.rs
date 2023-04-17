de5715dea7b5683851b58da85f0eceec0901cf59
wivl <68458084+wivl@users.noreply.github.com>
2023-04-17 20:15:02 +0800

    add: ray

---
+++ src/ray.rs
@@ -0,0 +1,24 @@
+#[macro_use]
+extern crate approx; // For the macro relative_eq!
+extern crate nalgebra as na;
+use na::{Point3, Vector3};
+
+#[derive(Debug, Clone, Copy)]
+pub struct Ray {
+    pub orig: Point3<f32>,
+    pub dir: Vector3<f32>,
+}
+
+impl Ray {
+    pub fn new(origin: Point3<f32>, direction: Vector<f32>) -> Self {
+        Ray {
+            orig: origin,
+            dir: direction
+        }
+    }
+
+    pub fn at(&self, t: f32) -> Point3<f32> {
+        self.orig + self.dir * t
+    }
+}
+
