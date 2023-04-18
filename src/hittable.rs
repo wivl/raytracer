92d487fe03146aa7fd67d01457a2edb890406b7e
wivl <68458084+wivl@users.noreply.github.com>
2023-04-18 23:54:24 +0800

    add: normal direction

--- src/hittable.rs
+++ src/hittable.rs
@@ -5,9 +5,19 @@ pub struct HitRecord {
     pub p: Point3<f32>,
     pub normal: Vector3<f32>,
     pub t: f32,
+    pub front_face: bool,
+}
+
+// if ray comes from outside, normal towards out; if from inside, normal towards in
+impl HitRecord {
+    pub fn set_face_normal(&mut self, r: &Ray, outward_normal: &Vector3<f32>) {
+        self.front_face = r.dir.dot(&outward_normal) < 0.0;
+        self.normal = if self.front_face { *outward_normal } else { -*outward_normal };
+    }
 }
 
 
+// define hittable trait
 pub trait Hittable {
     fn hit(&self, r: &Ray, t_min: f32, t_max: f32, rec: &mut HitRecord) -> bool;
 }
