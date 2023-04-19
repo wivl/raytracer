f6ae924d8664daf9f6cb1375d329bd550042c957
wivl <68458084+wivl@users.noreply.github.com>
2023-04-19 15:37:58 +0800

    fix: uv

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,11 @@ mod object;
 
 fn main() {
     // image
-    const WIDTH: u32 = 800;
-    const HEIGHT: u32 = 800;
-    let aspect: f32 = WIDTH as f32 / HEIGHT as f32;
+    let aspect: f32 = 16.0 / 9.0;
+    let width: u32 = 1080;
+    let height: u32 = (width as f32 / aspect + 0.5) as u32;
 
-    let mut image: ImageBuffer<Rgba<u8>, _> = ImageBuffer::new(WIDTH, HEIGHT);
+    let mut image: ImageBuffer<Rgba<u8>, _> = ImageBuffer::new(width, height);
 
     // camera
     let viewport_height: f32 = 2.0;
@@ -28,10 +28,12 @@ fn main() {
     let lower_left_corner: Point3<f32> = origin - horizontal/2.0 - vertical/2.0 - Vector3::new(0.0, 0.0, focal_length);
 
     // render
-    for h in 0..HEIGHT {
-        for w in 0..WIDTH {
-            let u = w as f32 / (WIDTH - 1) as f32;
-            let v = h as f32 / (WIDTH - 1) as f32;
+    for h in 0..height {
+        for w in 0..width {
+            let u = w as f32 / (width - 1) as f32;
+            let v = h as f32 / (height - 1) as f32;
+            // 将像素坐标系的原点从左上角转换为左下角，使得渲染结果与标准的图像坐标系一致。
+            // let v = (height - h - 1) as f32 / (height - 1) as f32;
             let r = Ray::new(origin, lower_left_corner + u*horizontal + v*vertical - origin);
             let pixel = ray_color(&r);
             println!("{:?}", pixel);
