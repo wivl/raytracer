87bb9fba6d4287abd6fb2676161ea4678dba9bdc
wivl <68458084+wivl@users.noreply.github.com>
2023-05-05 23:44:57 +0800

    add: gamma correct

--- src/antialiasing.cpp
+++ src/antialiasing.cpp
@@ -2,7 +2,9 @@
 
 void muliple_samples(Vector3f &intensity, int spp) {
     float scale = 1.0 / spp;
-    intensity.x() *= scale;
-    intensity.y() *= scale;
-    intensity.z() *= scale;
+    // gamma correct
+
+    intensity.x() = sqrt(intensity.x() * scale);
+    intensity.y() = sqrt(intensity.y() * scale);
+    intensity.z() = sqrt(intensity.z() * scale);
 }
