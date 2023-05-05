035365d8485015218173c219729b7862773063a2
wivl <68458084+wivl@users.noreply.github.com>
2023-05-05 23:45:20 +0800

    add: functions

--- src/rtmath.hpp
+++ src/rtmath.hpp
@@ -1,6 +1,8 @@
 #ifndef _RTMATH_HPP_
 #define _RTMATH_HPP_
 
+#include <Eigen/Dense>
+#include "src/Core/Matrix.h"
 #include <cmath>
 #include <limits>
 #include <memory>
@@ -18,10 +20,15 @@ inline float degrees_to_radians(float degrees) {
 }
 
 // generate float number in [0, 1)
-inline float random_double() {
+inline float random_float() {
     static std::uniform_real_distribution<float> distribution(0.0, 1.0);
     static std::mt19937 generator;
     return distribution(generator);
+}
+
+inline double random_float(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max-min)*random_float();
 }
 
 // clamp x in [min, max]
@@ -29,6 +36,36 @@ inline float clamp(float x, float min, float max) {
     if (x < min) return min;
     if (x > max) return max;
     return x;
+}
+
+inline Eigen::Vector3f v3f_random() {
+    return Eigen::Vector3f(random_float(), random_float(), random_float());
+}
+
+inline Eigen::Vector3f v3f_random(float min, float max) {
+    return Eigen::Vector3f(random_float(min,max), random_float(min,max), random_float(min,max));
+}
+
+inline Eigen::Vector3f random_in_unit_sphere() {
+    while (true) {
+        auto p = v3f_random(-1, 1);
+        if (p.dot(p) >= 1) {
+            continue;
+        }
+        return p;
+    }
+}
+
+inline Eigen::Vector3f random_unit_vector() {
+    return random_in_unit_sphere().normalized();
+}
+
+inline Eigen::Vector3f random_in_hemisphere(const Eigen::Vector3f &normal) {
+    Eigen::Vector3f in_unit_sphere = random_in_unit_sphere();
+    if (in_unit_sphere.dot(normal) > 0.0) // In the same hemisphere as the normal
+        return in_unit_sphere;
+    else
+        return -in_unit_sphere;
 }
 
 // Common Headers
